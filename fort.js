3285e877eb11d5acace6fca65c53f34e2bbd1eda
Idris <idris@colourity.com>
2014-05-11 13:58:07 -0400

    Create fort.js

---
+++ fort.js
@@ -0,0 +1,12 @@
+var inputs = $(".form :input").not('button, [type="button"], [type="submit"]').on({
+    input: cback
+});
+
+function cback() {
+    var b = inputs.filter(function () {
+            return !this.value.length
+        }).length,
+        a = inputs.length;
+    $(".congrats").show().text(b + " missing(Completed " + ((100 - (b / a) * 100).toFixed(2) + "%") + ")");
+    $(".top").css("width", 100 - (b / a) * 100 + "%")
+};
